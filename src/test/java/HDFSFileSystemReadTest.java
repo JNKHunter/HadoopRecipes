9be7cf5fe4ed95759405bbeebc1e1b8f680f9597
John Hunter <john@push-it-forward.com>
2016-12-26 18:28:27 -0800

    added filesystemreader test

--- src/test/java/HDFSFileSystemReadTest.java
+++ src/test/java/HDFSFileSystemReadTest.java
@@ -1,5 +1,10 @@
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import static org.junit.Assert.*;
 
@@ -7,14 +12,22 @@ import static org.junit.Assert.*;
  * Created by jhunter on 12/26/16.
  */
 public class HDFSFileSystemReadTest {
+    HDFSFileSystemRead fileSystemRead;
+    private String hdfsUrl = "hdfs://localhost:9000/quangle.txt";
+
     @Before
     public void setUp() throws Exception {
-
+        fileSystemRead = new HDFSFileSystemRead();
     }
 
     @Test
     public void getInputStream() throws Exception {
+        InputStream in = fileSystemRead.getInputStream(hdfsUrl);
+        InputStreamReader isr = new InputStreamReader(in);
+        BufferedReader br = new BufferedReader(isr);
+        String line = br.readLine();
 
+        assertEquals("quangle", line);
     }
 
 }
