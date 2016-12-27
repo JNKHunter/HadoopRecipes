193335b56cc5307924e7356139688756abded016
John Hunter <john@push-it-forward.com>
2016-12-26 18:05:51 -0800

    added getinputstream method

--- src/main/java/HDFSFileSystemRead.java
+++ src/main/java/HDFSFileSystemRead.java
@@ -1,4 +1,10 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 
 /**
  * Created by jhunter on 12/26/16.
@@ -6,8 +12,16 @@ import org.apache.hadoop.conf.Configuration;
 public class HDFSFileSystemRead {
 
 
+    InputStream getInputStream(String hdfsUrl) throws IOException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(hdfsUrl), conf);
+        InputStream in = null;
 
-    Configuration conf;
+        return fs.open(new Path(hdfsUrl));
+
+    }
+
+
 
 
 }
