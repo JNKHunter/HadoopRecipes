a958acc139866d8a47441f90223b8ea6873827b3
John Hunter <john@push-it-forward.com>
2016-12-28 16:31:03 -0800

    updated tests

--- src/test/java/FileCopyTest.java
+++ src/test/java/FileCopyTest.java
@@ -7,14 +7,18 @@ import static org.junit.Assert.*;
  * Created by jhunter on 12/28/16.
  */
 public class FileCopyTest {
+
+    FileCopy fileCopy;
+
     @Before
     public void setUp() throws Exception {
-
+        fileCopy = new FileCopy("longtext.txt",
+                "hdfs://localhost:9000/user/jhunter/filecopy/longtext.txt");
     }
 
     @Test
     public void copyFile() throws Exception {
-
+        fileCopy.copyFile();
     }
 
 }
