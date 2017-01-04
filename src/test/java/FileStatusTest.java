8a586fc4fb4ca706eabdf82f31c63dce313626dd
John Hunter <john@push-it-forward.com>
2017-01-03 17:31:07 -0800

    Added setup method"

--- src/test/java/FileStatusTest.java
+++ src/test/java/FileStatusTest.java
@@ -1,5 +1,32 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * Created by jhunter on 1/3/17.
  */
 public class FileStatusTest {
+
+    private MiniDFSCluster cluster;
+    private FileSystem fs;
+
+    @Before
+    public void setUp() throws IOException {
+        Configuration conf = new Configuration();
+        if (System.getProperty("test.build.data") == null ){
+            System.setProperty("test.build.data", "/tmp");
+        }
+
+        cluster = new MiniDFSCluster.Builder(conf).build();
+        fs = cluster.getFileSystem();
+        OutputStream out = fs.create(new Path("/dir/file"));
+        out.write("content".getBytes("UTF-8"));
+        out.close();
+    }
+
 }
