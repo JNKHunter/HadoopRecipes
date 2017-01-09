4e0837a7707bb240d0c0692aa49001edb8ede8f7
John Hunter <john@push-it-forward.com>
2017-01-08 16:45:42 -0800

    added filepath tests

--- src/main/java/ListStatus.java
+++ src/main/java/ListStatus.java
@@ -1,5 +1,34 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+
 /**
  * Created by jhunter on 1/8/17.
  */
 public class ListStatus {
+
+    private String[] pathStrings;
+    private Path[] paths;
+    private FileSystem fs;
+    private Configuration conf;
+
+    public ListStatus(String fsPathString, String[] pathStrings) throws Exception {
+        this.pathStrings = pathStrings;
+        conf = new Configuration();
+        fs = FileSystem.get(URI.create(fsPathString), conf);
+        initPaths();
+    }
+
+    private void initPaths(){
+        paths = new Path[pathStrings.length];
+        for(int i = 0; i < pathStrings.length; i++){
+            paths[i] = new Path(pathStrings[i]);
+        }
+    }
+
+    public Path[] getPaths() {
+        return paths;
+    }
 }
---
+++ src/test/java/ListStatusTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by jhunter on 1/8/17.
+ */
+public class ListStatusTest {
+
+}
